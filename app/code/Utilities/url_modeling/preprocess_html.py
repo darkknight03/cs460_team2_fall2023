44c191703f26d931db2d68e85821c94bb9d910cf
Xu, Mazer <mingzex2@illinois.edu>
2023-11-08 17:40:26 -0600

    update html model & dump db

--- app/code/Utilities/url_modeling/preprocess_html.py
+++ app/code/Utilities/url_modeling/preprocess_html.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib3
-
-
+import re
 
 def preprocess_html(html):
     soup = BeautifulSoup(html, "html.parser")
     text = soup.get_text()
+    text = re.sub('[\n]+', '\n', text)
     return text
+
 def read_html(url):
     http = urllib3.PoolManager()
     html_doc = http.request('GET', url)
