c76544bfd229c7fe824f96f57a067a09b1e0f5a6
Xinyi Wei <73124063+xwei16@users.noreply.github.com>
2023-11-25 23:18:43 -0600

    Update url.py

--- url_analysis/url.py
+++ url_analysis/url.py
@@ -1,23 +1,22 @@
 import re
 import whois
 from datetime import datetime
-import request
+import requests
 
 # Define heuristics and their weights
 # ref: https://drive.google.com/file/d/1gGAMknkZAiK8m01vtpQJebwuTdoxAMVP/view?usp=sharing, pp.26
 heuristics = {
-    'Domain Age': 10,
-    'URL Length': 5,
-    'Contains IP Address': 8,
-    'SSL Certificate': 10,
-    'Keyword Match': 7
+    'Domain Age': 8,
+    'URL Length': 4,
+    'Contains IP Address': 1,
+    'SSL Certificate': 2
 }
 
 # Sample URL for analysis
 url = "https://example-phishing-url.com/login.php"
 
 # Define functions to calculate criteria values
-def get_domain_age(url):
+def domain_age(url):
     try:
         # Extract the domain from the URL
         domain = url.split("//")[-1].split("/")[0]
@@ -35,26 +34,33 @@ def get_domain_age(url):
         if creation_date:
             today = datetime.now()
             age = (today - creation_date).days
+            # if age of domain < 6 months
+            if ((age/30) < 6):
+              age = 1
+            else:
+              age = 0
             return age
-        else:
-            return None
-
+    
     except Exception as e:
-        print(f"Error: {e}")
+        print("dang")
         return None
 
 # Get the domain age
-domain_age = get_domain_age(url)
+domain_age = domain_age(url)
 
-if domain_age is not None:
-    print(f"The domain age of '{url}' is {domain_age} days.")
-else:
-    print(f"Unable to retrieve domain age for '{url}'.")
-
+#if domain_age is not None:
+#    print(f"The domain age of '{url}' is {domain_age} days.")
+#else:
+#    print(f"Unable to retrieve domain age for '{url}'.")
+#
 
 
 def get_url_length(url):
-    return len(url)
+    if len(url) < 54:
+        length = 0
+    else:
+        length = 1
+    return length
 
 def contains_ip_address(url):
     return int(bool(re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', url)))
@@ -76,20 +82,22 @@ def has_ssl_certificate(url):
 def keyword_match(url):
     return int("login" in url)  # 1 if keyword exists, 0 if not
 
+
+
 # Calculate criteria values
 criteria = {
     'Domain Age': get_domain_age(url),
     'URL Length': get_url_length(url),
     'Contains IP Address': contains_ip_address(url),
-    'SSL Certificate': has_ssl_certificate(url),
-    'Keyword Match': keyword_match(url)
+    'SSL Certificate': has_ssl_certificate(url)
+    #'Keyword Match': keyword_match(url)
 }
 
 # Calculate the phishing score
 phishing_score = sum(heuristics[heuristic] * criteria[heuristic] for heuristic in heuristics)
 
 # Define a threshold (adjust as needed)
-threshold = 50
+threshold = 8
 
 # Determine if the URL is potentially phishing
 is_phishing = phishing_score < threshold
